5ae4dda1fd2fcb87134917c7b429e3fcf46e2d49
Alex Ballera <alexballera@gmail.com>
2017-02-03 21:57:39 -0400

    Instalado WP , WooCommerce Storefront y Child

--- tasks/images.js
+++ tasks/images.js
@@ -6,7 +6,7 @@ import imageminSvgo from 'imagemin-svgo'
 import imageminOptipng from 'imagemin-optipng'
 import imageminJpegtran from 'imagemin-jpegtran'
 
-gulp.task('images', () => {
+gulp.task('images', ['screenshot', 'login'], () => {
   return gulp.src('./src/images/**')
     .pipe(cache(imagemin({
       optimizationLevel: 7,
@@ -27,4 +27,46 @@ gulp.task('images', () => {
     })))
     .pipe(gulp.dest('./build/images'))
     .pipe(gulp.dest('./colorglob/images'))
+})
+gulp.task('screenshot', () => {
+  gulp.src('./src/screenshot.png')
+    .pipe(cache(imagemin({
+      optimizationLevel: 7,
+      progressive: true,
+      interlaced: true,
+      multipass: true,
+      use: [
+        pngquant(),
+        imageminSvgo(),
+        imageminOptipng({optimizationLevel: 7}),
+        imageminJpegtran({progressive: true})
+      ],
+      svgoPlugins: [
+        { removeViewBox: false }, // don't remove the viewbox atribute from the SVG
+        { removeUselessStrokeAndFill: false }, // don't remove Useless Strokes and Fills
+        { removeEmptyAttrs: false } // don't remove Empty Attributes from the SVG
+      ]
+    })))
+    .pipe(gulp.dest('./colorglob'))
+})
+gulp.task('login', () => {
+  gulp.src('./src/login/*.*g')
+    .pipe(cache(imagemin({
+      optimizationLevel: 7,
+      progressive: true,
+      interlaced: true,
+      multipass: true,
+      use: [
+        pngquant(),
+        imageminSvgo(),
+        imageminOptipng({optimizationLevel: 7}),
+        imageminJpegtran({progressive: true})
+      ],
+      svgoPlugins: [
+        { removeViewBox: false }, // don't remove the viewbox atribute from the SVG
+        { removeUselessStrokeAndFill: false }, // don't remove Useless Strokes and Fills
+        { removeEmptyAttrs: false } // don't remove Empty Attributes from the SVG
+      ]
+    })))
+    .pipe(gulp.dest('./colorglob/login'))
 })
